132462c5a48e52869462c1a67f2d83d22c5e776a
lebuu997 <lebuu997@gmail.com>
2019-03-26 10:13:24 +0700

    khoi tao du an

---
+++ lab04_05/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="lab04_05.MvcApplication" Language="C#" %>
---
+++ lab04_05/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(lab04_05.Startup))]
+namespace lab04_05
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
